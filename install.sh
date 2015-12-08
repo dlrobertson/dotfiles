d7243857eb1f000606c63df6cd41060f121ac151
Daniel Robertson <danlrobertson89@gmail.com>
2015-12-08 17:26:38 -0500

    Add install script
    "Install" script will only symbolicly link the file to the config files
    necessary location
    
    Signed-off-by: Daniel Robertson <danlrobertson89@gmail.com>

---
+++ install.sh
@@ -0,0 +1,7 @@
+cur_wd=$(pwd)
+ln -svf ${cur_wd}/muttrc $HOME/.muttrc
+ln -svf ${cur_wd}/vimrc $HOME/.vimrc
+ln -svf ${cur_wd}/xinitrc $HOME/.xinitrc
+ln -svf ${cur_wd}/Xmodmap $HOME/.Xmodmap
+ln -svf ${cur_wd}/tmux $HOME/.tmux
+ln -svf ${cur_wd}/tmux.conf $HOME/.tmux.conf

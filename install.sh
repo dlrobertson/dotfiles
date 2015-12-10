9ab2f861fe707631dbaadf34cc1534b8af6d74c6
Daniel Robertson <danlrobertson89@gmail.com>
2015-12-10 17:26:49 -0500

    Add more trickery to the install script
    Signed-off-by: Daniel Robertson <danlrobertson89@gmail.com>

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 cur_wd=$(pwd)
-ln -svf ${cur_wd}/muttrc $HOME/.muttrc
 ln -svf ${cur_wd}/vimrc $HOME/.vimrc
+vim +PluginInstall +qall
 ln -svf ${cur_wd}/xinitrc $HOME/.xinitrc
 ln -svf ${cur_wd}/Xmodmap $HOME/.Xmodmap
-ln -svf ${cur_wd}/tmux $HOME/.tmux
-ln -svf ${cur_wd}/tmux.conf $HOME/.tmux.conf
-ln -svf ${cur_wd}/zshrc $HOME/.zshrc
+if [ hash mutt 2> /dev/null ]; then
+    ln -svf ${cur_wd}/muttrc $HOME/.muttrc
+fi
+if [ hash tmux 2> /dev/null ]; then
+    ln -svf ${cur_wd}/tmux $HOME/.tmux
+    ln -svf ${cur_wd}/tmux.conf $HOME/.tmux.conf
+fi
+if [ hash zsh 2> /dev/null ]; then
+    ln -svf ${cur_wd}/zshrc $HOME/.zshrc
+fi
